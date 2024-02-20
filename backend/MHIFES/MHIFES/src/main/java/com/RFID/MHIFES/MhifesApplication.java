3bcc40ab476ba1af8ef25175419ae4c79ecf9fec
gustavo.nunes <105016559+voidGustavoNunes@users.noreply.github.com>
2024-02-20 11:56:58 -0300

    Primeiro commit

---
+++ backend/MHIFES/MHIFES/src/main/java/com/RFID/MHIFES/MhifesApplication.java
@@ -0,0 +1,13 @@
+package com.RFID.MHIFES;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhifesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhifesApplication.class, args);
+	}
+
+}
---
+++ backend/MHIFES/MHIFES/src/test/java/com/RFID/MHIFES/MhifesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RFID.MHIFES;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhifesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
