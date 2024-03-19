135f18ef64d16db717f2476c9f89d70ef8f0ace1
lalasp18 <laspio18@gmail.com>
2024-03-18 21:30:48 -0300

    tabela estendida de pessoa para aluno, professor, coordenador

--- backend/MHIFES/src/main/java/com/RFID/MHIFES/model/Pessoa.java
+++ backend/MHIFES/src/main/java/com/RFID/MHIFES/model/Pessoa.java
@@ -5,12 +5,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
 @Table(name = "pessoa")
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Pessoa {
 
     @Id
