0a09f7e0fa81cccfeac25ca0abffdcae95a564b4
lalasp18 <laspio18@gmail.com>
2024-04-03 16:44:29 -0300

    id auto coordenadoria

--- backend/MHIFES/src/main/java/com/RFID/MHIFES/model/Coordenadoria.java
+++ backend/MHIFES/src/main/java/com/RFID/MHIFES/model/Coordenadoria.java
@@ -2,6 +2,8 @@ package com.rfid.mhifes.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -12,6 +14,7 @@ import lombok.Data;
 public class Coordenadoria {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(length = 150, nullable = false)
