20b0d22b4bf4c0264b7c2aba8458b115aca9ac9c
Edson Lima <49738290+EdsonLima99@users.noreply.github.com>
2024-04-04 11:32:09 -0300

    Atualizar local quase pronto

--- backend/MHIFES/src/main/java/com/RFID/MHIFES/service/LocalService.java
+++ backend/MHIFES/src/main/java/com/RFID/MHIFES/service/LocalService.java
@@ -14,6 +14,7 @@ import com.rfid.mhifes.repository.EquipamentoRepository;
 import com.rfid.mhifes.repository.LocalEquipamentoRepository;
 import com.rfid.mhifes.repository.LocalRepository;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -22,24 +23,20 @@ import jakarta.validation.constraints.Positive;
 @Validated
 public class LocalService extends GenericServiceImpl<Local, LocalRepository> {
 
-    private final LocalEquipamentoRepository localEquipamentoRepository;
-    private final EquipamentoRepository equipamentoRepository;
+    private final LocalEquipamentoService localEquipamentoService;
 
-    public LocalService(LocalRepository localRepository, LocalEquipamentoRepository localEquipamentoRepository,
-            EquipamentoRepository equipamentoRepository) {
+    public LocalService(LocalRepository localRepository, LocalEquipamentoService localEquipamentoService) {
         super(localRepository);
-        this.localEquipamentoRepository = localEquipamentoRepository;
-        this.equipamentoRepository = equipamentoRepository;
+        this.localEquipamentoService = localEquipamentoService;
     }
 
     @Override
     public Local criar(@Valid @NotNull Local local) {
         Local localNovo = repository.save(local);
 
-        for(LocalEquipamento localEquipamento : local.getLocalEquipamentos()) {
+        for (LocalEquipamento localEquipamento : local.getLocalEquipamentos()) {
             localEquipamento.setLocal(local);
-            localEquipamento.setEquipamento(equipamentoRepository.findById(localEquipamento.getEquipamento().getId()).get());
-            localEquipamentoRepository.save(localEquipamento);
+            localEquipamentoService.criar(localEquipamento);
         }
 
         return localNovo;
@@ -51,7 +48,22 @@ public class LocalService extends GenericServiceImpl<Local, LocalRepository> {
                 .map(localEditado -> {
                     localEditado.setNome(local.getNome());
                     localEditado.setCapacidade(local.getCapacidade());
-                    localEditado.setLocalEquipamentos(local.getLocalEquipamentos());
+
+                    for (LocalEquipamento localEquipamento : localEditado.getLocalEquipamentos()) {
+                        if(!local.getLocalEquipamentos().contains(localEquipamento)){
+                            localEquipamentoService.excluir(localEquipamento.getId());
+                        }
+                    }
+
+                    for(LocalEquipamento localEquipamento : local.getLocalEquipamentos()){
+                        if(localEditado.getLocalEquipamentos().contains(localEquipamento)){
+                            localEquipamentoService.atualizar(localEquipamento.getId(), localEquipamento);
+                        } else {
+                            localEquipamento.setLocal(localEditado);
+                            localEquipamentoService.criar(localEquipamento);
+                        }
+                    }
+
                     return repository.save(localEditado);
                 }).orElseThrow(() -> new RegistroNotFoundException(id));
     }
---
+++ backend/MHIFES/src/main/java/com/rfid/mhifes/service/LocalEquipamentoService.java
@@ -0,0 +1,39 @@
+package com.rfid.mhifes.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+import com.rfid.mhifes.exception.RegistroNotFoundException;
+import com.rfid.mhifes.model.Equipamento;
+import com.rfid.mhifes.model.Local;
+import com.rfid.mhifes.model.LocalEquipamento;
+import com.rfid.mhifes.repository.EquipamentoRepository;
+import com.rfid.mhifes.repository.LocalEquipamentoRepository;
+import com.rfid.mhifes.repository.LocalRepository;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+@Service
+@Validated
+public class LocalEquipamentoService extends GenericServiceImpl<LocalEquipamento, LocalEquipamentoRepository> {
+
+    public LocalEquipamentoService(LocalEquipamentoRepository localEquipamentoRepository) {
+        super(localEquipamentoRepository);
+    }
+
+    @Override
+    public LocalEquipamento atualizar(@NotNull @Positive Long id, @Valid @NotNull LocalEquipamento entity) {
+        return repository.findById(id)
+                .map(localEquipamento -> {
+                    localEquipamento.setQuantidade(entity.getQuantidade());
+                    return repository.save(localEquipamento);
+                }).orElseThrow(() -> new RegistroNotFoundException(id));
+    }
+
+}
